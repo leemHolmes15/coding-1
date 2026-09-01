01500e0bcd1775be84a7d5beef56e5df37525172
leemHolmes15 <sholmes826@boyslatin.net>
2026-09-01 12:45:10 +0000

    dateypes

---
+++ UNIT 1/datetype.py
@@ -0,0 +1,11 @@
+# sring is a block of characters wrapped in quotes
+#charaters can be number letters or symbols 
+# INTEGERS- datetypes that are just whole numbers or numbers
+#With decimal points.
+# we dont have to add any spacial symbols we can write the number
+# as it.
+#FLOATS-A number with a decimal
+#BOOLEAN- A date types that can only be either true or false 
+#we
+false
+True 
