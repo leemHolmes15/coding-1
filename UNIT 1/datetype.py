a107231eab5c41f344ec4a9fdfc1f53dc6c129b6
leemHolmes15 <sholmes826@boyslatin.net>
2026-09-03 12:49:52 +0000

    saleem holmes

--- UNIT 1/datetype.py
+++ UNIT 1/datetype.py
@@ -1,11 +1,20 @@
 # sring is a block of characters wrapped in quotes
 #charaters can be number letters or symbols 
-# INTEGERS- datetypes that are just whole numbers or numbers
-#With decimal points.
+
+# INTEGERS- datetypes that are 
+# just whole numbers or numbers  With decimal points.
 # we dont have to add any spacial symbols we can write the number
 # as it.
+
 #FLOATS-A number with a decimal
-#BOOLEAN- A date types that can only be either true or false 
-#we
+
+#BOOLEAN- A date types that can only be either
+#boolean values. the computer understand these words 
+# as something unique.
+
+# the hashtag is a marker that makes anything written
+# behind it invisible to the computer.
+
 false
-True 
+True
+
---
+++ UNIT 1/veriableActivity.py
@@ -0,0 +1,78 @@
+#variable- a container that stores date. you can
+#name your container anything you want so long 
+#as it is  dexscriptive 
+
+# Variables operate on the priniciple of Key / Value pairs
+
+
+user = 'Saleem ' # a variable storing a string
+yearsOfExperience = 10 # a variable storing a number
+graduated = True # a variable storing a boolean
+
+Ja3SneakersCount = 9
+Ja3SneakersPrice = 150.00
+Ja3SneakersSKU = "91022382913"
+
+Douglas42="" 
+S21average="" 
+
+
+# When we want to create a variable, we make up a word, 
+# use the equal sign and then assign it a data type.  
+# whenever the computer sees our variable word it will
+# alway use the data inside. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+yearsofExp= 10
+graduate= True
+
+Douglas42= '' 
+Savage21= ''
+thomas3 =''
+j2ordan3= ''
+
+age = 10
+Age = 20
+AGE = 30
+
+print(age)
+print(AGE)
+print(Age)
+
+
+
+
+
+
+
+
+
+
+24 red apple counted in the store INTEGERS
+" numer of  kid in the math class" STRING 
+The trip cost 102.99 dollers FLOTS
+age:15= True BOOLEAN
+
+
+
+
+
+#variable Namin Rule
+# A varaible MUST start with letter and cannot start with a number 
+# A varaible can be mixdof letter and number BUT not symbols 
+# a Varaible 
