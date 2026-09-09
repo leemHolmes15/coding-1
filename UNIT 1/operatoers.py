b622738cc07f6e100459c18ed2a44f031636b07f
leemHolmes15 <sholmes826@boyslatin.net>
2026-09-09 12:55:34 +0000

    operator class notes

---
+++ UNIT 1/operatoers.py
@@ -0,0 +1,28 @@
+# A operators  Exclusively used to
+# assign values to varibales 
+# key/ Value pairing 
+
+# we use a single equal sign represent the assingment 
+# operators
+name = Saleem
+grade = 10th 
+school = trues 
+
+# Arithmetic Operators used on numerical 
+# date types to perform calculations 
+# intgers ( whole number ) and floats ( decimal numbers )
+
+# rint is a function  that let us show code 
+# in the teminal 
+
+print (3+3)# addtions operators 
+print (3+3)# subtraction operators 
+print (12/3)# division operators
+print (3*4) multiplication operators 
+
+# comparison operators set of symbols used 
+# to assess if date is the same or different and 
+# how they differ
+
+print (10> 1) # graeter then operator 
+print (10< 2) # less then operator 
